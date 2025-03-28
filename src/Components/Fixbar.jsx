3b5520fd805a3a3e47c9da98675c65fe87661af0
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-03-28 22:33:20 +0500

    Port-Folio

--- src/Components/Fixbar.jsx
+++ src/Components/Fixbar.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Fixbar = () => {
   return (
     <div>
-      <div className="">Fix Bar</div>
+      <h1>ok</h1>
+      <div className="">
+        <p>hi im abubakar</p>
+      </div>
+      <div className="">
+        <h1>ok</h1>
+      </div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+        maiores vitae ipsum repellat quas sit quam reprehenderit quos sint odio
+        omnis obcaecati sapiente, unde iste eos sed optio asperiores in?
+      </p>
     </div>
   );
 };
