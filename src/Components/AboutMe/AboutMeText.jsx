21594f6d5b265a08f756cf47ecc27988b02b57d5
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-04-07 18:33:13 +0500

    Portfolio

--- src/Components/AboutMe/AboutMeText.jsx
+++ src/Components/AboutMe/AboutMeText.jsx
@@ -4,7 +4,18 @@ const AboutMeText = () => {
   return (
     <div>
       <h2>About Me</h2>
-      <p></p>
+      <p>
+        I'm a passionate Front-End Web Developer with a strong focus on creating
+        visually appealing, responsive, and high-performance websites. With
+        experience in building modern user interfaces using technologies like
+        HTML, CSS, JavaScript, and React, I specialize in turning ideas into
+        functional, interactive digital experiences. I also integrate essential
+        tools like Klaviyo for email automation, optimize websites for mobile
+        performance, and ensure seamless plugin integrations. Whether you're a
+        brand looking to stand out or a business aiming for better user
+        engagement, I bring clean code, creative thinking, and a user-first
+        approach to every project.
+      </p>
     </div>
   );
 };
