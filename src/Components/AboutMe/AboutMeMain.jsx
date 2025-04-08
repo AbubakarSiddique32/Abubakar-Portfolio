d87edfb1bd512dab9c4ca3e8aa6d26207ed6172a
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-04-08 16:59:02 +0500

    Portfolio

--- src/Components/AboutMe/AboutMeMain.jsx
+++ src/Components/AboutMe/AboutMeMain.jsx
@@ -3,7 +3,7 @@ import AboutMeText from "./AboutMeText";
 import AboutMeImage from "./AboutMeImage";
 const AboutMeMain = () => {
   return (
-    <div className="flex md:flex-row sm:flex-col mx-w-[1200px] px-4 gap-12 mx-auto mt-[100px] justify-between items-center">
+    <div className="flex md:flex-row sm:flex-col max-w-[1200px] px-4 gap-12 mx-auto mt-[100px] justify-between items-center">
       <div>
         <AboutMeText />
       </div>
