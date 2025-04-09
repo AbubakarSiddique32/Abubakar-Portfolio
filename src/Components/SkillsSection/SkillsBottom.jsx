97d69f14537216fd4f9f1348778e6707878a5989
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-04-09 21:15:50 +0500

    Portfolio

--- src/Components/SkillsSection/SkillsBottom.jsx
+++ src/Components/SkillsSection/SkillsBottom.jsx
@@ -6,6 +6,12 @@ const SkillsBottom = () => {
     <div className=" border-y-2 border-lightGrey relative">
       <div className=" absolute bg-gradient-to-r from-orange to-cyan opacity-50 h-full w-full"></div>
       <img src={Image} alt="SKills Bottom" />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        incidunt veritatis assumenda, commodi expedita, in molestiae similique,
+        iusto exercitationem aspernatur earum veniam soluta voluptas quidem.
+        Libero incidunt doloribus ipsum numquam.
+      </p>
     </div>
   );
 };
